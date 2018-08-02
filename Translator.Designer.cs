771ae5a7f83987b2e139dc838a2309edcf003f88
Thai Khac Nguyen <shiroemon279@gmail.com>
2018-08-02 18:33:40 +0700

    Added the source code for the translator

---
+++ Translator.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Extrathicc
+{
+    partial class Translator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Translator));
+            this.translatorLabel = new System.Windows.Forms.Label();
+            this.englishTextBox = new System.Windows.Forms.TextBox();
+            this.translateButton = new System.Windows.Forms.Button();
+            this.extrathiccTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // translatorLabel
+            // 
+            this.translatorLabel.AutoSize = true;
+            this.translatorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.translatorLabel.Location = new System.Drawing.Point(225, 9);
+            this.translatorLabel.Name = "translatorLabel";
+            this.translatorLabel.Size = new System.Drawing.Size(313, 25);
+            this.translatorLabel.TabIndex = 0;
+            this.translatorLabel.Text = "EXTRATHICC TRANSLATOR";
+            // 
+            // englishTextBox
+            // 
+            this.englishTextBox.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.englishTextBox.Location = new System.Drawing.Point(20, 48);
+            this.englishTextBox.Multiline = true;
+            this.englishTextBox.Name = "englishTextBox";
+            this.englishTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.englishTextBox.Size = new System.Drawing.Size(740, 146);
+            this.englishTextBox.TabIndex = 1;
+            this.englishTextBox.Text = "enter your english text here";
+            // 
+            // translateButton
+            // 
+            this.translateButton.Location = new System.Drawing.Point(278, 200);
+            this.translateButton.Name = "translateButton";
+            this.translateButton.Size = new System.Drawing.Size(208, 53);
+            this.translateButton.TabIndex = 2;
+            this.translateButton.Text = "Translate";
+            this.translateButton.UseVisualStyleBackColor = true;
+            this.translateButton.Click += new System.EventHandler(this.translateButton_Click);
+            // 
+            // extrathiccTextBox
+            // 
+            this.extrathiccTextBox.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.extrathiccTextBox.Location = new System.Drawing.Point(20, 259);
+            this.extrathiccTextBox.Multiline = true;
+            this.extrathiccTextBox.Name = "extrathiccTextBox";
+            this.extrathiccTextBox.ReadOnly = true;
+            this.extrathiccTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.extrathiccTextBox.Size = new System.Drawing.Size(740, 146);
+            this.extrathiccTextBox.TabIndex = 3;
+            this.extrathiccTextBox.Text = "enter your extrathicc text here";
+            // 
+            // Translator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.extrathiccTextBox);
+            this.Controls.Add(this.translateButton);
+            this.Controls.Add(this.englishTextBox);
+            this.Controls.Add(this.translatorLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Translator";
+            this.Text = "Extrathicc translator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label translatorLabel;
+        private System.Windows.Forms.TextBox englishTextBox;
+        private System.Windows.Forms.Button translateButton;
+        private System.Windows.Forms.TextBox extrathiccTextBox;
+    }
+}
+
---
+++ Translator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Extrathicc
+{
+    public partial class Translator : Form
+    {
+        public Translator()
+        {
+            InitializeComponent();
+        }
+
+        private void translateButton_Click(object sender, EventArgs e)
+        {
+            string current = englishTextBox.Text;
+            string translation = TranslateToExtrathicc(current);
+
+            extrathiccTextBox.Clear();
+            extrathiccTextBox.Text = translation;
+        }
+
+        private string TranslateToExtrathicc(string enteredText)
+        {
+            Dictionary<string, string> translationTable = new Dictionary<string, string>()
+                                                      {
+                                                        {"a", "卂"}, {"b", "乃"}, {"c", "匚"}, {"d", "刀"},
+                                                        { "e", "乇"}, {"f", "下"}, {"g", "厶"}, {"h", "卄"},
+                                                        { "i", "工"}, {"j", "丁"}, {"k", "长"}, {"l", "乚"},
+                                                        { "m", "从"}, {"n", "𠘨"}, {"o", "口"}, {"p", "尸"},
+                                                        { "q", "㔿"}, {"r", "尺"}, {"s", "丂"}, {"t", "丅"},
+                                                        { "u", "凵"}, {"v", "リ"}, {"w", "山"}, {"x", "乂"},
+                                                        { "y", "丫"}, {"z", "乙"}
+                                                     };
+
+            StringBuilder translatedText = new StringBuilder();
+            foreach (char letter in enteredText)
+            { 
+                if (char.IsLetter(letter))
+                {
+                    translatedText.Append(translationTable[letter.ToString()]);
+                }
+                else
+                {
+                    translatedText.Append(letter);
+                }
+            }
+            return translatedText.ToString();
+        }
+    }
+}
